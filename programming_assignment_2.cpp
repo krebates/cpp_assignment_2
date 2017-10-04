e8d759ea753766ad44fe8c7096168a11adc93b60
Krystle <krebates@gmail.com>
2017-10-04 16:02:07 -0500

    junk

--- programming_assignment_2.cpp
+++ programming_assignment_2.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <algorithm>
+#include <fstream.h>
 
 using namespace std;
 
@@ -18,6 +20,17 @@ int main(){
     count++;
     cout<<item<<endl;
   }
+
+  https://www.thecrazyprogrammer.com/2011/07/c-program-to-read-from-text-file-and.html
+  cout.open("sample.txt"); //create file to write
+    char ch;
+    while(cin) //loop wiill run till end of file
+      {
+        cin>>ch;       //reading data from file
+        cout<<ch;       //writing data to file
+      }
+    cin.close();
+    cout.close();
 
   cout << count << " items found."<<endl;
 
