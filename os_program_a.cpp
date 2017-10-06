8b1d98e0da1de7c2d76d5d233179fe268b7de525
Krystle <krebates@gmail.com>
2017-10-06 13:10:52 -0500

    reads in variables and provides the 2nd varible given

--- os_program_a.cpp
+++ os_program_a.cpp
@@ -11,48 +11,62 @@ using namespace std;
 int main(int argc, char* argv[]) {
 
    //shows how arguments work
-   // cout << "argc = " << argc << endl;
-   // for(int i = 0; i < argc; i++)
-   //    cout << "argv[" << i << "] = " << argv[i] << endl;
-   // return 0;
+   cout << "argc = " << argc << endl;
+   for(int i = 0; i < argc; i++)
+      cout << "argv[" << i << "] = " << argv[i] << endl;
 
 
-  int pipe_file_desc[2];
-  int process_id;
-  char buffer[20];
 
+  // int pipe_file_desc[2];
+  // int process_id;
+  // char buffer[20];
+
+
+  printf("%s", argv[1]);
   ifstream inFile;
-  inFile.open("test2.txt");
+  inFile.open(argv[1]);
 
   // string item;
+  // int count = 0;
+
+  // //Read a file until the end is reached
+  // while(!inFile.eof()){
+  //   inFile >> item;
+  //   count++;
+  //   cout<<item<<endl;
+  // }
+
+  // cout << count << " items found."<<endl;
 
 
-  if (pipe(pipe_file_desc) == -1)
-  {
-    perror ("error creating");
-    exit(0);
-  }
 
-  process_id = fork();
-  if (process_id == -1)
-    {perror ("error creating");
-   // printf("pid in parent=%d and childid=%d\n",getpid(),pid);
-    exit(0);
-  }
 
-  if (process_id == 0)
-  {
-    close(pipe_file_desc[1]);
-    read(pipe_file_desc[0], buffer, 20);
-    printf("%s\n", buffer);
-     // printf("pid in child=%d and parent=%d\n",getpid(),getppid());
-    close(pipe_file_desc[0]);
-  }
-  else
-  {
-    close(pipe_file_desc[0]);
-    write(pipe_file_desc[1], "hi", 20);
-    close(pipe_file_desc[1]);
-  }
+  // if (pipe(pipe_file_desc) == -1)
+  // {
+  //   perror ("error creating");
+  //   exit(0);
+  // }
 
+  // process_id = fork();
+  // if (process_id == -1)
+  //   {perror ("error creating");
+  //  // printf("pid in parent=%d and childid=%d\n",getpid(),pid);
+  //   exit(0);
+  // }
+
+  // if (process_id == 0)
+  // {
+  //   close(pipe_file_desc[1]);
+  //   read(pipe_file_desc[0], buffer, 20);
+  //   printf("%s\n", buffer);
+  //    // printf("pid in child=%d and parent=%d\n",getpid(),getppid());
+  //   close(pipe_file_desc[0]);
+  // }
+  // else
+  // {
+  //   close(pipe_file_desc[0]);
+  //   write(pipe_file_desc[1], "hi", 20);
+  //   close(pipe_file_desc[1]);
+  // }
+return 0;
 }
