e1cdc722cc859ab7baca1c34b35baf2b7a161ffb
Krystle <krebates@gmail.com>
2017-10-06 11:07:42 -0500

    pipe example works

---
+++ pipe_example.cpp
@@ -0,0 +1,39 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+  int pipe_file_desc[2];
+  int process_id;
+  char buffer[20];
+
+
+  if (pipe(pipe_file_desc) == -1)
+  {
+    perror ("error creating");
+    exit(0);
+  }
+  process_id = fork();
+  if (process_id == -1)
+    {perror ("error creating");
+   // printf("pid in parent=%d and childid=%d\n",getpid(),pid);
+
+    exit(0);
+  }
+  if (process_id == 0)
+  {
+    close (pipe_file_desc[1]);
+    read (pipe_file_desc[0], buffer, 20);
+    printf("%s\n", buffer);
+     // printf("pid in child=%d and parent=%d\n",getpid(),getppid());
+    close(pipe_file_desc[1]);
+  }
+  else
+  {
+    close(pipe_file_desc[0]);
+    write(pipe_file_desc[1], "hi", 20);
+    close(pipe_file_desc[1]);
+  }
+
+}
