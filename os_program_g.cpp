cf36e08554a834558aad7166f356d2827db54180
Krystle <krebates@gmail.com>
2017-10-09 22:30:51 -0500

    formatting part 2

--- os_program_g.cpp
+++ os_program_g.cpp
@@ -26,18 +26,18 @@ int main ( int argc, char *argv[] )
             printf("Error");
             exit(1);
         }
-    if (pid > 0) {  /* parent process */
-    close(pipe_file_desc_2[WRITE]);        //close write end, read, and then close read end
-    read(pipe_file_desc_2[READ],&readValue,sizeof(readValue));
-    close(pipe_file_desc_2[READ]);
-      }
-      else { /* child process */
-        close(pipe_file_desc_2[READ]);       //close read end, write and then close write end
-        writeValue+=valueToAdd;
-        write(pipe_file_desc_2[WRITE],&writeValue,sizeof(writeValue));
-        printf("child: writeValue value : %d\n", writeValue);
-        close(pipe_file_desc_2[WRITE]);
-      }
+        if (pid > 0) { /* parent process */
+            close(pipe_file_desc_2[WRITE]);        //close write end, read, and then close read end
+            read(pipe_file_desc_2[READ],&readValue,sizeof(readValue));
+            close(pipe_file_desc_2[READ]);
+        }
+        else { /* child process */
+            close(pipe_file_desc_2[READ]);       //close read end, write and then close write end
+            writeValue+=valueToAdd;
+            write(pipe_file_desc_2[WRITE],&writeValue,sizeof(writeValue));
+            printf("child: writeValue value : %d\n", writeValue);
+            close(pipe_file_desc_2[WRITE]);
+        }
 
     }
 
