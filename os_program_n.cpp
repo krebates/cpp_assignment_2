0bd729690683cfc7567a1073802e5834dd0eb66e
Krystle <krebates@gmail.com>
2017-10-11 17:32:34 -0500

    Pipe is working

--- os_program_n.cpp
+++ os_program_n.cpp
@@ -20,16 +20,49 @@ int main ( int argc, char *argv[] )
   int a = atoi(argv[2]);
   int numProcesses = atoi(argv[2]);
   char buffer[20];
- static int writeValue = 50;
- static string newNumber;
+  int writeValue = 50;
   int readValue;
-  string line, word, name;
+  string item, newWord, word, line;
   int count = 0;
 
-  //Opens up the file to read
-  char b[73] = "";
-  ifstream f(argv[1]);
+  // //Opens up the file to read
+  // char b[73] = "";
+  // ifstream readText;
+  // ifstream f(argv[1]);
 
-  f.read(b, sizeof(b) - 1); // Read one less that sizeof(b) to ensure null
-  cout << b;
+  // // while(!readText.eof()){
+  //   f.read(b, sizeof(b) - 1); // Read one less that sizeof(b) to ensure null
+  //   cout << b;
+  // // }
+
+  // int num_lines = 0;
+  // std::string input;
+  // char b[73] = "";
+
+  // for (; b < 10 && getline(input); ++num_lines)
+  // {
+  //     // do-whatever
+  // }
+
+  for(i = 0; i < numProcesses; i++) {
+    pid = fork();
+    if(pid < 0) {
+      printf("Error");
+      exit(0);
+    }
+    if (pid > 0) { /* parent process */
+      wait(0);
+      close(pipe_file_desc_2[WRITE]);
+      read(pipe_file_desc_2[READ],&readValue,sizeof(readValue));
+      close(pipe_file_desc_2[READ]);
+      exit(0);
+    }
+    else { /* child process */
+        close(pipe_file_desc_2[READ]);
+        writeValue+=valueToAdd;
+        write(pipe_file_desc_2[WRITE],&writeValue,sizeof(writeValue));
+        close(pipe_file_desc_2[WRITE]);
+    }
+  }
+ cout << writeValue << endl;
 }
