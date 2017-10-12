150ac63826d20ce11541bc766a0166f37f1f9460
Krystle <krebates@gmail.com>
2017-10-12 16:32:22 -0500

    puts in spaces

--- os_program_n.cpp
+++ os_program_n.cpp
@@ -13,7 +13,7 @@ int pipe_function(int valueGiven, int numProcesses, int valueToAdd){
   int pid;
   int pipe_file_desc_2[2];
 
-  for(int i = 0; i < numProcesses - 1; i++)
+  for(int i = 0; i < (numProcesses - 1); i++)
   {
     if (pipe(pipe_file_desc_2) == -1)
     {
@@ -29,11 +29,12 @@ int pipe_function(int valueGiven, int numProcesses, int valueToAdd){
     }
 
     if (pid > 0) { /* parent process */
-      wait(0);
+      waitpid(0, NULL, 0);
       close(pipe_file_desc_2[WRITE]);
       read(pipe_file_desc_2[READ],&valueGiven,sizeof(valueGiven));
       close(pipe_file_desc_2[READ]);
       exit(0);
+
     }
 
     else { /* child process */
@@ -49,28 +50,9 @@ int pipe_function(int valueGiven, int numProcesses, int valueToAdd){
 int main ( int argc, char *argv[] )
 {
   string item, newitem;
+  int position;
 
 
-  // //---------
-  // //Opens up the file to read for 72 characters
-  // char b[73] = "";
-  // ifstream readText;
-  // ifstream f(argv[1]);
-
-  // // while(!readText.eof()){
-  //   f.read(b, sizeof(b) - 1); // Read one less that sizeof(b) to ensure null
-  //   cout << b;
-  // // }
-  // //---------
-
-  // int num_lines = 0;
-  // std::string input;
-  // char b[73] = "";
-
-  // for (; b < 10 && getline(input); ++num_lines)
-  // {
-  //     // do-whatever
-  // }
 
     //Opens up the file to read
       ifstream readText;
@@ -82,32 +64,43 @@ int main ( int argc, char *argv[] )
 
     while(!readText.eof())
     {
-    string nums = "";
-      readText >> item;
+      string nums = "";
+        readText >> item;
+        for(int i=0; i < item.length(); ++i)
+        {
+          if (isdigit(item[i]) && !isdigit(item[i+1])){
+            nums += item[i];
+            int num = atoi(nums.c_str());
+            int changednums = pipe_function(num, atoi(argv[2]), atoi(argv[3]) );
+            string stringnum = to_string(changednums);
+            text += stringnum;
+           nums = "";
+          }
+          else if (isdigit(item[i])){
+            nums += item[i];
 
-      for(int i=0; i < item.length(); ++i){
-        if (isdigit(item[i]) && !isdigit(item[i+1])){
-          nums += item[i];
-          int num = atoi(nums.c_str());
-          int changednums = pipe_function(num, atoi(argv[3]), atoi(argv[2]) );
-          string stringnum = to_string(changednums);
-          text += stringnum;
-         nums = "";
-        }
-        else if (isdigit(item[i])){
-          nums += item[i];
+          }
+          else
+            text += item[i];
 
         }
-        else if (isspace(item[i])){
           text += " ";
-        }
-        else
-          text += item[i];
-
-        }
       }
+
     cout << text << endl;
 
+  // while(text.size() > 0)
+  // {
+  //   position = count 72
+  //   if(text.at(position) == " ")
+  //   {
+  //     text = text.substring;
+  //   }
+  //   else{
+  //     temp = substring;
+  //     position = temp.found_last_of(" ")
+  //   }
+  // }
 
   return 0;
 }
